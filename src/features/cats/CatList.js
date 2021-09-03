fb2ddce66b06ebf712e42f1e3d6b77b891421d5c
ahmed <a.alrahoo2001@gmail.com>
2021-09-03 13:44:49 +0300

    Done.

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,9 @@
-// write your CatList component here
+const CatList = ({catPics}) => {
+    return(
+        <div>
+            {catPics.map(e=><img src={e.url} alt="cat"></img>)}
+        </div>
+    )
+}
+
+export default CatList
